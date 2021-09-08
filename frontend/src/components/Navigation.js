100680645951782eb1a09590c708401a8e886a2c
Andres Jurado <os1990ju@gmail.com>
2021-09-08 06:51:46 -0500

    finaliza el nav-bar de la app

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,28 +1,35 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 function Navigation() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+ <div className="container">
+ <Link className="navbar-brand" to ="/">
+    NotesApp
+    </Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
+    <ul className="navbar-nav ms-auto">
+    <li className="nav-item active">
+        <Link className="nav-link" to="/">Note List </Link>
+        
+      </li>
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <Link className="nav-link" to="/edit/:id">Edit Note </Link>
+        
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
+        <Link className="nav-link" to="/create">Create Note</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
+        <Link className="nav-link" to="/user">Create User</Link>
       </li>
     </ul>
   </div>
+ </div>
+  
 </nav>
     )
 }
--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+const url = 'http://localhost/api/user';
 function NoteList() {
-    return (
-        <div>
-            List of notes
-        </div>
-    )
+    const res = await axios.get(url);
+    console.log(res);
+        const { loading, products } = useFetch(url)
+  console.log(products)
+  return (
+    <div>
+      <h2>{loading ? 'loading...' : 'data'}</h2>
+    </div>
+  )
 }
 
-export default NoteList
+export default NoteList;
+
+
+
+
+
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+
+
